b91cb6fd7c9333e3759ae9e794946875c3629682
Romina <parchamimahdimohammad@gmail.com>
2022-07-01 00:21:28 +0430

    Update DataBase.java

---
+++ src/database/DataBase.java
@@ -0,0 +1,63 @@
+package database;
+
+import utils.Categoris;
+import utils.Item;
+import utils.Order;
+import utils.User;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.*;
+import java.util.stream.Collectors;
+
+// singleton class
+public class DataBase {
+    static DataBase database;
+    ReadFromFile readFromFile = new ReadFromFile();
+    WriteToFile writeToFile = new WriteToFile();
+    DataToString dataToString = new DataToString();
+    public Map<Integer, Item> items = new HashMap<Integer, Item>();
+    public Map<Integer, User> users;
+    ArrayList<Order> orders;
+    List<Categoris> categories;
+
+
+    private DataBase() {
+        for (Item i : readFromFile.readItems("E:\\Aria\\T2\\AP\\ServerForOnlineShop\\src\\data\\Items.txt")) {
+            items.put(i.id, i);
+        }
+        categories = readFromFile.readCategories("E:\\Aria\\T2\\AP\\ServerForOnlineShop\\src\\data\\Categories.txt");
+        users = readFromFile.readAllUsers("E:\\Aria\\T2\\AP\\ServerForOnlineShop\\src\\data\\Users.txt");
+    }
+
+    public static DataBase getInstance() {
+        if (database == null) {
+            database = new DataBase();
+        }
+        return database;
+    }
+
+    List<Item> getItems() {
+        return new ArrayList<>(items.values());
+    }
+
+    List<Categoris> getCategories() {
+        if (categories == null) {
+            return categories = readFromFile.readCategories("E:\\Aria\\T2\\AP\\ServerForOnlineShop\\src\\data\\Categories.txt");
+        }
+        return categories;
+    }
+
+    public static void main(String[] args) {
+        database = DataBase.getInstance();
+        //test users
+    }
+
+    public List<User> getUsers() {
+        return new ArrayList<>(users.values());
+    }
+}
+
